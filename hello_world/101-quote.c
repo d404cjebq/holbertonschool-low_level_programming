ceaa2bb293bebc98f176347472a8980d73d2248f
d404cjebq <imdhaydhw@gmail.com>
2026-02-25 03:08:22 +0000

    add 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -o quote -Wall -Werror -W
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
