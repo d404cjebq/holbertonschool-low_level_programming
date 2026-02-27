4c5828baa56d52c4008adedf200899091ed3101c
d404cjebq <imdhaydhw@gmail.com>
2026-02-27 17:14:09 +0000

    add 101-natural.c

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	i = 0;
+	sum = 0;
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
