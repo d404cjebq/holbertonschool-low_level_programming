2e2ffdd15ac329f4ed60e773a4be9cdb38e558b5
d404cjebq <imdhaydhw@gmail.com>
2026-02-28 21:43:52 +0000

    add 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+ int _isupper(int c)
+{
+	if (c >= '0' && c <= 'a')
+		return (1);
+	return (0);
+}
