0eaed71d6157c467a6ddc852a72bb988cec59384
d404cjebq <imdhaydhw@gmail.com>
2026-03-01 21:31:43 +0000

    Add 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,13 +16,9 @@ void more_numbers(void)
 		n = 0;
 		while (n <= 14)
 		{
-			if (n >= 10)
-			{
+			if (n > 9)
 				_putchar('1');
-				_putchar('0' + n - 10);
-			}
-			else
-				_putchar('0' + n);
+			_putchar('0' + n % 10);
 			n++;
 		}
 		_putchar('\n');
