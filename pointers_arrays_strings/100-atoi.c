510465729bf05b8ccf6d52f21da2115bd58eb8f7
d404cjebq <imdhaydhw@gmail.com>
2026-03-02 17:52:49 +0000

    add 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,27 +11,27 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int result = 0;
-	int started = 0;
 
-	while (s[i] != '\0')
+	/* handle signs */
+	while (s[i] == '+' || s[i] == '-')
 	{
-		/* count signs before number */
-		if (s[i] == '-' && !started)
+		if (s[i] == '-')
 			sign *= -1;
+		i++;
+	}
 
-		/* if digit */
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			started = 1;
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (started)
-		{
-			break;
-		}
+	/* convert digits */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		int digit = s[i] - '0';
+
+		if (sign == 1)
+			result = result * 10 + digit;
+		else
+			result = result * 10 - digit;
 
 		i++;
 	}
 
-	return (result * sign);
+	return (result);
 }
