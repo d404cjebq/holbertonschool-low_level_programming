9056c8a82c3a960775b4662483d2519191af791b
d404cjebq <imdhaydhw@gmail.com>
2026-03-05 17:32:44 +0000

    add 101-keygen.c

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a valid password for 101-crackme
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    srand(time(NULL));
+    /* بعد تحليل البرنامج تضيف المنطق هنا */
+    return (0);
+}
