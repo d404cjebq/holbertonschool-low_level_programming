8733eaf087020beead508b99dac9caebbe9ff37d
d404cjebq <imdhaydhw@gmail.com>
2026-03-05 17:50:22 +0000

    Fix keygen

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,48 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 
 /**
- * generate_password - Generates a random password that would be valid
- * 
- * Return: pointer to generated password string
- */
-char *generate_password(void)
-{
-    static char password[12];
-    int i;
-    int sum = 0;
-    char *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    
-    /* Initialize random seed */
-    srand(time(NULL));
-    
-    /* Generate 8 character password */
-    for (i = 0; i < 8; i++)
-    {
-        password[i] = charset[rand() % 62];
-        sum += password[i];
-    }
-    
-    /* Add a checksum character to make it valid */
-    password[8] = charset[sum % 62];
-    password[9] = '\0';
-    
-    return password;
-}
-
-/**
- * main - Entry point for keygen
- * 
- * Return: Always 0
+ * main - generates a valid password for 101-crackme
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-    char *password;
-    
-    password = generate_password();
-    printf("%s", password);
-    
-    return (0);
+	int i;
+
+	/* 2772 / 12 = 231 لكن كبير، نستخدم حروف متكررة */
+	/* 'z' = 122, نحتاج 2772 / 122 = ~22 حرف */
+	for (i = 0; i < 22; i++)
+		printf("%c", 'z');
+
+	/* 2772 - (22 * 122) = 2772 - 2684 = 88 = 'X' */
+	printf("%c\n", 'X');
+	return (0);
 }
