6d6f27b91203ad7614705ba1e3ade51f6cc28141
d404cjebq <imdhaydhw@gmail.com>
2026-03-05 17:19:25 +0000

    add 100-prime_factor.c

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -3,31 +3,28 @@
 
 int main(void)
 {
-    long long n = 612852475143;
-    long long largest = -1;
-    long long i;
+    double n = 612852475143.0;
+    double largest = -1;
+    double i;
 
-    /* نقسم على 2 أول */
-    while (n % 2 == 0)
+    while (fmod(n, 2) == 0)
     {
         largest = 2;
         n /= 2;
     }
 
-    /* نجرب الأعداد الفردية */
     for (i = 3; i <= sqrt(n); i += 2)
     {
-        while (n % i == 0)
+        while (fmod(n, i) == 0)
         {
             largest = i;
             n /= i;
         }
     }
 
-    /* إذا باقي عدد أكبر من 1 فهو أولي */
     if (n > 1)
         largest = n;
 
-    printf("%lld\n", largest);
+    printf("%.0f\n", largest);
     return (0);
 }
