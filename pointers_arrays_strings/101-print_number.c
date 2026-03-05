7d1c8f79ef2a5f33efaf19f7c0fa9ebfbe758124
d404cjebq <imdhaydhw@gmail.com>
2026-03-05 19:57:03 +0000

    add 101-print_number.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -9,20 +9,20 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        if (n == -2147483648)
-        {
-            print_number(214748364);
-            _putchar('8');
-            return;
-        }
-        n = -n;
-    }
-    
-    if (n / 10)
-        print_number(n / 10);
-    
-    _putchar(n % 10 + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n == -2147483648)
+		{
+			print_number(214748364);
+			_putchar('8');
+			return;
+		}
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
 }
