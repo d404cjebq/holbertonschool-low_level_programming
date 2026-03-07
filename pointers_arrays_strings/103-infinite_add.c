54824d44b41a82f5cc335295053867ef4ed8bb31
d404cjebq <imdhaydhw@gmail.com>
2026-03-07 17:54:58 +0000

    add 103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,70 +1,50 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * infinite_add - Adds two numbers as strings and stores result in buffer
- * @n1: First number as string
- * @n2: Second number as string
- * @r: Buffer to store result
- * @size_r: Size of buffer
+ * infinite_add - adds two number strings and stores result in buffer
+ * @n1: first number string
+ * @n2: second number string
+ * @r: result buffer
+ * @size_r: size of result buffer
  *
- * Return: Pointer to result, or 0 if result can't be stored
+ * Return: pointer to result, or 0 if buffer too small
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l1, l2, carry, sum, digit, max_len, start;
+	int len1, len2, carry, i, j, k, sum;
 
-	/* Get lengths of both numbers */
-	for (l1 = 0; n1[l1] != '\0'; l1++)
-		;
-	for (l2 = 0; n2[l2] != '\0'; l2++)
-		;
+	len1 = strlen(n1);
+	len2 = strlen(n2);
 
-	/* Determine maximum length needed for result */
-	max_len = (l1 > l2 ? l1 : l2) + 1;
-
-	/* Check if result can fit in buffer */
-	if (max_len >= size_r)
+	/* Need at most max(len1, len2) + 1 digits + null terminator */
+	if (size_r <= (len1 > len2 ? len1 : len2) + 1)
 		return (0);
 
-	/* Initialize buffer */
-	for (i = 0; i <= max_len; i++)
-		r[i] = '\0';
-
-	/* Start addition from the end of strings */
-	i = l1 - 1;
-	j = l2 - 1;
-	k = max_len - 1;
 	carry = 0;
+	i = len1 - 1;    /* index into n1, right to left */
+	j = len2 - 1;    /* index into n2, right to left */
+	k = size_r - 2;  /* index into r, right to left (leave room for '\0') */
 
-	/* Perform addition digit by digit */
-	while (i >= 0 || j >= 0 || carry > 0)
+	r[size_r - 1] = '\0';
+
+	while (i >= 0 || j >= 0 || carry)
 	{
 		sum = carry;
 
 		if (i >= 0)
-		{
-			sum += n1[i] - '0';
-			i--;
-		}
-
+			sum += n1[i--] - '0';
 		if (j >= 0)
-		{
-			sum += n2[j] - '0';
-			j--;
-		}
+			sum += n2[j--] - '0';
 
-		digit = sum % 10;
 		carry = sum / 10;
 
-		r[k] = digit + '0';
-		k--;
+		if (k < 0)
+			return (0);
+
+		r[k--] = (sum % 10) + '0';
 	}
 
-	/* Find start position (skip leading zeros) */
-	start = 0;
-	while (r[start] == '0' && r[start + 1] != '\0')
-		start++;
-
-	/* Return pointer to correct position */
-	return (r + start);
+	/* Return pointer to where the number starts in the buffer */
+	return (&r[k + 1]);
 }
