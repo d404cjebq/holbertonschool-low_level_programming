dbd34f111387bf139c2e38f0356ed9c0787b05da
d404cjebq <imdhaydhw@gmail.com>
2026-03-09 17:35:33 +0000

    add 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,23 +1,39 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: the base number
- * @y: the exponent
+ * _sqrt_helper - helper function to find square root recursively
+ * @n: the number to find square root for
+ * @i: the current guess (starting from 0)
  *
- * Return: x raised to power y, or -1 if y is negative
+ * Return: natural square root of n, or -1 if not found
  */
-int _pow_recursion(int x, int y)
+int _sqrt_helper(int n, int i)
 {
-	if (y < 0)
+	if (i * i > n)
 	{
-		return (-1);  /* Error: negative exponent */
+		return (-1);  /* No natural square root found */
 	}
 
-	if (y == 0)
+	if (i * i == n)
 	{
-		return (1);   /* Base case: any number to power 0 is 1 */
+		return (i);   /* Found the square root */
 	}
 
-	return (x * _pow_recursion(x, y - 1));
+	return (_sqrt_helper(n, i + 1));  /* Try next number */
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find square root for
+ *
+ * Return: natural square root of n, or -1 if n is negative or has no root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);  /* Error: negative number */
+	}
+
+	return (_sqrt_helper(n, 0));
 }
