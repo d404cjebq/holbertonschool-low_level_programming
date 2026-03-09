a9b68f7636bb10faaabaab3a2807005ad62c0b4a
d404cjebq <imdhaydhw@gmail.com>
2026-03-09 18:00:07 +0000

    add 100-change.c

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * calculate_coins - calculates minimum number of coins using while loop
+ * calculate_coins - calculates minimum number of coins
  * @cents: amount in cents
  *
  * Return: minimum number of coins needed
@@ -7,35 +10,52 @@
 int calculate_coins(int cents)
 {
 	int coins = 0;
+	int coin_values[] = {25, 10, 5, 2, 1};
+	int i;
 
-	while (cents > 0)
+	for (i = 0; i < 5; i++)
 	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else
-		{
-			cents -= 1;
-			coins++;
-		}
+		coins += cents / coin_values[i];
+		cents %= coin_values[i];
 	}
 
 	return (coins);
+}
+
+/**
+ * main - prints the minimum number of coins to make change
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins;
+
+	/* Check if exactly one argument is passed */
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* Convert argument to integer */
+	cents = atoi(argv[1]);
+
+	/* If amount is negative, print 0 */
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	/* Calculate minimum number of coins */
+	coins = calculate_coins(cents);
+
+	/* Print the result */
+	printf("%d\n", coins);
+
+	return (0);
 }
