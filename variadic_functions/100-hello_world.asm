146141862e06a44ade4f46644d4c98533636473e
d404cjebq <imdhaydhw@gmail.com>
2026-04-05 13:00:03 +0000

    add a

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,15 @@
+section .data
+    msg db "Hello, World", 0x0a
+
+section .text
+    global main
+
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 13
+    syscall
+    mov rax, 60
+    xor rdi, rdi
+    syscall
