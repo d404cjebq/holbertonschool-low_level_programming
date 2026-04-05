8916ad1b25d6af9b2b7476f3737198e024bf056c
d404cjebq <imdhaydhw@gmail.com>
2026-04-05 13:02:40 +0000

    add

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -10,32 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
-    unsigned char *main_ptr;
+	int num_bytes, i;
+	unsigned char *main_ptr;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    num_bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
 
-    main_ptr = (unsigned char *)main;
+	main_ptr = (unsigned char *)main;
 
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x", main_ptr[i]);
-        if (i < num_bytes - 1)
-            printf(" ");
-    }
-    printf("\n");
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+		if (i < num_bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
 
-    return (0);
+	return (0);
 }
