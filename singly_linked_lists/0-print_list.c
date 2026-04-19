655c2d4a7abcc3c75148ca6167b5d0b61c82a29a
d404cjebq <imdhaydhw@gmail.com>
2026-04-19 14:43:44 +0000

    add

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t count = 0;
    if(h -> str == NULL)
 	   printf("[0] (nil)\n");
    else
-	   printf("[%u] %s\n",h -> len ,h -> str );
+	   printf("[%u] %s\n", h -> len , h -> str );
            count++;
-          h =h ->next;
+          h = h -> next;
    }
    return(count);
    }
